ab4d452c247fcb02ec8bf95ab6cc589e659af016
Hussam <nirolive134@gmail.com>
2026-09-18 01:46:26 +0300

    fix the ui

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home({
 }) {
   const isAdmin = (await searchParams)?.admin === "true";
   return (
-    <div className="flex h-screen">
+    <div className="flex min-h-screen">
       {isAdmin && (
         <PassKeyModel/>
       )}
